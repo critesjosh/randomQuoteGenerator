65f1ccb480a56b7560c921fafb6f488021bc5e44
Josh <critesjosh@gmail.com>
2016-07-05 21:25:20 -0600

    Added animation to the display
    I added a fade in/out as the quotes change. Also cleaned up app.js.

--- app.js
+++ app.js
@@ -1,37 +1,42 @@
 var author = '';
 var quote = '';
-var colors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6',
- '#FB6964', '#342224', "#472E32", "#BDBB99", "#77B1A9", "#73A857"];
 
-$.getJSON( "https://jsonp.afeld.me/?callback=&url=http://quotes.stormconsultancy.co.uk/random.json", {
-  }).done(function(data){
-  console.log(data);
-  author = data.author;
-  quote = data.quote;
+var getQuote = function() {
+  $.getJSON( "https://jsonp.afeld.me/?callback=&url=http://quotes.stormconsultancy.co.uk/random.json", {
+    }).done(function(data){
+    console.log(data);
+    author = data.author;
+    quote = data.quote;
 
-  $("#quote").replaceWith('<p class="quote"><i class="fa fa-quote-left" aria-hidden="true"></i> ' + quote + '</p>');
-  $("#author").replaceWith('<p class="author"> + ' + author + '</p>');
-  $("#twitter").attr('href', 'https://twitter.com/intent/tweet?text=' + encodeURIComponent('"' + quote + '" ' + author));
+    $("#quote").animate({
+      opacity: 0
+    }, 500,
+    function() {
+      $(this).animate({
+        opacity: 1
+      }, 500);
+      $("#quote").html('<i class="fa fa-quote-left" aria-hidden="true"></i> ' + quote);
+    });
 
-});
+    $("#author").animate({
+      opacity: 0
+    }, 500,
+    function() {
+      $(this).animate({
+        opacity: 1
+      }, 500);
+      $("#author").text('+ ' + author);
+      });
+
+    $("#twitter").attr('href', 'https://twitter.com/intent/tweet?text=' + encodeURIComponent('"' + quote + '" ' + author));
+  });
+}
+
+getQuote();
 
 $(document).ready(function() {
 
     $("#new-quote").on("click", function(){
-
-      $.getJSON( "https://jsonp.afeld.me/?callback=&url=http://quotes.stormconsultancy.co.uk/random.json", {
-        format: 'jsonp'
-      }).done(function(data){
-        console.log(data);
-        author = data.author;
-        quote = data.quote;
-
-
-        $(".quote").replaceWith('<p class="quote"><i class="fa fa-quote-left" aria-hidden="true"></i> ' + quote + '</p>');
-        $(".author").replaceWith('<p class="author"> + ' + author + '</p>');
-        $("#twitter").attr('href', 'https://twitter.com/intent/tweet?text=' + encodeURIComponent('"' + quote + '" ' + author));
-
+        getQuote();
       });
     });
-
-});
